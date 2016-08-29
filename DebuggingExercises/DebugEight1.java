1a894a918eb481a263b8750b4ca0c267ca37b4be
kyecook <kye.cook@my.jcu.edu.au>
2016-08-29 10:31:02 +1000

    Started debugging exercises

--- DebuggingExercises/DebugEight1.java
+++ DebuggingExercises/DebugEight1.java
@@ -9,7 +9,7 @@ public class DebugEight1
       char userCode;
       String entry, message;
       boolean found = false;
-      char[] okayCodes = {'A''C''T''H'};
+      char[] okayCodes = {'A','C','T','H'};
       StringBuffer prompt = new 
           StringBuffer("Enter shipping code for this delivery\nValid codes are: ");
       for(int x = 0; x <= okayCodes.length; ++x)
@@ -31,7 +31,7 @@ public class DebugEight1
       if(found)
          message = "Good code";
       else
-         message = "Sorry code not found;
+         message = "Sorry code not found";
       JOptionPane.showMessageDialog(null, message);
    }
 }
